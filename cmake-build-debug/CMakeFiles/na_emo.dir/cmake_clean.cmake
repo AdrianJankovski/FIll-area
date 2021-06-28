90010752c3e1d2d08b48f222cab87be57db58ed3
emosharov1234 <86207599+emosharov1234@users.noreply.github.com>
2021-06-28 14:12:49 +0300

    Add makefiles

---
+++ cmake-build-debug/CMakeFiles/na_emo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/na_emo.dir/main.cpp.obj"
+  "libna_emo.dll.a"
+  "na_emo.exe"
+  "na_emo.exe.manifest"
+  "na_emo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/na_emo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
